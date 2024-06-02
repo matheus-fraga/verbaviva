b73f9bea4565804234ab0c9eafb4c00e2c428069
matheus-fraga <matheuscarlosfraga@gmail.com>
2024-06-01 22:35:47 -0300

    crud endereço finalizado

--- src/main/java/com/verbaviva/projeto/controllers/EnderecoController.java
+++ src/main/java/com/verbaviva/projeto/controllers/EnderecoController.java
@@ -1,33 +1,61 @@
 package com.verbaviva.projeto.controllers;
 
+import com.verbaviva.projeto.entities.Endereco;
+
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
+import com.verbaviva.projeto.services.EnderecoService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.verbaviva.projeto.entities.Endereco;
-import com.verbaviva.projeto.repositories.EnderecoRepository;
 
 @RestController
-@RequestMapping(value = "/endereco")
+@RequestMapping(value = "/enderecos")
 public class EnderecoController {
+
 	@Autowired
-	private EnderecoRepository repository;
-	
+	private EnderecoService service;
+
 	@GetMapping
 	public ResponseEntity<List<Endereco>> findAll() {
-		List<Endereco> list = repository.findAll();
+		List<Endereco> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Optional<Endereco>> findById(@PathVariable Long id) {
-		Optional<Endereco> obj = repository.findById(id);
+	public ResponseEntity<Endereco> findById(@PathVariable Long id) {
+		Endereco obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	public ResponseEntity<Endereco> insert(@RequestBody Endereco obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Endereco> update(@PathVariable Long id, @RequestBody Endereco obj) {
+		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
 	}
 }
