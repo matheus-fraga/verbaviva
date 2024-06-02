7b83320360174134244ee5022d1847730b021adc
matheus-fraga <matheuscarlosfraga@gmail.com>
2024-06-01 22:23:51 -0300

    pequna correção na entidade usuário

--- src/main/java/com/verbaviva/projeto/entities/Usuario.java
+++ src/main/java/com/verbaviva/projeto/entities/Usuario.java
@@ -2,18 +2,18 @@ package com.verbaviva.projeto.entities;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-// import java.util.ArrayList;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-
-// import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-// import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -25,18 +25,23 @@ public class Usuario implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
+
 	private String cpf;
+
 	@Column(name = "data_nascimento")
 	private LocalDate dataNascimento;
 
-	// @JsonIgnore
-	// @OneToMany(mappedBy = "usuario")
-	// private List<Votos> votos = new ArrayList<>();
+	@JsonIgnore
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true) 
+	// cascade: ao excluir um usuário, todos os votos associados a esse usuário serão excluídos também.
+	private List<Voto> votos = new ArrayList<>();
 
-	// @JsonIgnore
-	// @OneToMany(mappedBy = "usuario")
-	// private List<Projetos> projetos = new ArrayList<>();
+	@JsonIgnore
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+	// cascade: ao excluir um usuário, todos os projetos associados a esse usuário serão excluídos.
+	private List<Projeto> projetos = new ArrayList<>();
 
 	public Usuario() {
 	}
@@ -82,36 +87,36 @@ public class Usuario implements Serializable {
 		this.dataNascimento = dataNascimento;
 	}
 
-	// public List<Votos> getVotos() {
-	// return votos;
-	// }
+	public List<Voto> getVotos() {
+	return votos;
+	}
 
-	// public List<Projetos> getProjetos() {
-	// return projetos;
-	// }
+	public List<Projeto> getProjetos() {
+	return projetos;
+	}
 
-	// @Override
-	// public int hashCode() {
-	// 	final int prime = 31;
-	// 	int result = 1;
-	// 	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	// 	return result;
-	// }
+	@Override
+	public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	return result;
+	}
 
-	// @Override
-	// public boolean equals(Object obj) {
-	// 	if (this == obj)
-	// 		return true;
-	// 	if (obj == null)
-	// 		return false;
-	// 	if (getClass() != obj.getClass())
-	// 		return false;
-	// 	Usuario other = (Usuario) obj;
-	// 	if (id == null) {
-	// 		if (other.id != null)
-	// 			return false;
-	// 	} else if (!id.equals(other.id))
-	// 		return false;
-	// 	return true;
-	// }
+	@Override
+	public boolean equals(Object obj) {
+	if (this == obj)
+	return true;
+	if (obj == null)
+	return false;
+	if (getClass() != obj.getClass())
+	return false;
+	Usuario other = (Usuario) obj;
+	if (id == null) {
+	if (other.id != null)
+	return false;
+	} else if (!id.equals(other.id))
+	return false;
+	return true;
+	}
 }
